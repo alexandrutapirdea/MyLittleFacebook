c4f221e53ca0c0cf96b3f04e1626ec197fd03244
= <ALEXANDRU.TAPIRDEA@my.fmi.unibuc.ro>
2018-12-27 11:25:02 +0200

    First commit

---
+++ MyLittleFacebook/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using MyLittleFacebook.Models;
+
+namespace MyLittleFacebook.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shadowhunters"};
+
+//            return View(movie);
+            return HttpNotFound();
+        }
+
+        public ActionResult Index(int? cm, string Name)
+        {
+            if (!cm.HasValue)
+                cm = 15;
+            if (String.IsNullOrWhiteSpace(Name))
+                Name = "JohnG";
+
+            return Content("Hello");
+        }
+    }
+}
